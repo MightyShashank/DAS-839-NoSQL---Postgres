f9891373f87ff0b079a3f374b41b6e9bf26e5f71
= <=>
2025-02-11 04:20:33 +0530

    Syntactical changes in bulkload_tables.sql

--- sql/bulkload_tables.sql
+++ sql/bulkload_tables.sql
@@ -3,10 +3,7 @@
 -- Enabling timing to show duration of query execution
 \timing on
 
-\COPY keywords(id, term, score)
-FROM 'Wikipedia-EN-20120601_KEYWORDS.TSV'
-WITH (FORMAT CSV, DELIMITER E'\t', ENCODING 'UTF8');
+\COPY uris(id, uri) FROM 'Wikipedia-EN-20120601_REVISION_URIS.TSV' WITH (FORMAT CSV, DELIMITER E'\t', ENCODING 'UTF8');
 
-\COPY uris(id, uri)
-FROM 'Wikipedia-EN-20120601_REVISION_URIS.TSV'
-WITH (FORMAT CSV, DELIMITER E'\t', ENCODING 'UTF8');
+\COPY keywords(id, term, score) FROM 'Wikipedia-EN-20120601_KEYWORDS.TSV' WITH (FORMAT CSV, DELIMITER E'\t', ENCODING 'UTF8');
+

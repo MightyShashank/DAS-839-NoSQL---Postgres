6686f169b4acd0afb6b4df8c4ec943706c2fe746
= <=>
2025-02-10 17:54:47 +0530

    Added sql/bulkload_tables.sql

---
+++ sql/bulkload_tables.sql
@@ -0,0 +1,12 @@
+-- The script must be run with the root of the project as the root of the psql for the file paths to work
+
+-- Enabling timing to show duration of query execution
+\timing on
+
+\COPY keywords(id, term, score)
+FROM 'Wikipedia-EN-20120601_KEYWORDS.TSV'
+WITH (FORMAT CSV, DELIMITER E'\t', ENCODING 'UTF8');
+
+\COPY uris(id, uri)
+FROM 'Wikipedia-EN-20120601_REVISION_URIS.TSV'
+WITH (FORMAT CSV, DELIMITER E'\t', ENCODING 'UTF8');

77fa2d8e56c373f8fe94949faa2ccd9fd261ade0
Shashank Tippanavar <152605527+MightyShashank@users.noreply.github.com>
2025-02-14 20:50:30 +0530

    Update bulkload_tables.sql

--- sql/bulkload_tables.sql
+++ sql/bulkload_tables.sql
@@ -3,7 +3,7 @@
 -- Enabling timing to show duration of query execution
 \timing on
 
-\COPY uris(id, uri) FROM 'Wikipedia-EN-20120601_REVISION_URIS.TSV' WITH (FORMAT CSV, DELIMITER E'\t', ENCODING 'UTF8');
+\COPY uris(id, url) FROM '/mnt/c/Users/shash/OneDrive/Desktop/NoSQL Assignments/Wikipedia-EN-20120601_REVISION_URIS.TSV' WITH (FORMAT CSV, DELIMITER E'\t', ENCODING 'UTF8');
 
-\COPY keywords(id, term, score) FROM 'Wikipedia-EN-20120601_KEYWORDS.TSV' WITH (FORMAT CSV, DELIMITER E'\t', ENCODING 'UTF8');
+\COPY keywords(id, term, score) FROM '/mnt/c/Users/shash/OneDrive/Desktop/NoSQL Assignments/Wikipedia-EN-20120601_KEYWORDS.TSV' WITH (FORMAT CSV, DELIMITER E'\t', ENCODING 'UTF8');
 

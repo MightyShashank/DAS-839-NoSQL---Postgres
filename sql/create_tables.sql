8fcebd0d1f3c970b1b0821986246314d7805a86b
Shashank Tippanavar <152605527+MightyShashank@users.noreply.github.com>
2025-02-14 21:04:00 +0530

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,15 +1,16 @@
+TRUNCATE TABLE keywords;
+TRUNCATE TABLE uris;
+
 CREATE TABLE keywords (
     id INTEGER NOT NULL,
     term VARCHAR(50) NOT NULL,
     score FLOAT NOT NULL,
     PRIMARY KEY (id, term)
 );
-
 CREATE TABLE uris (
     id INTEGER NOT NULL,
     url VARCHAR(500) NOT NULL,
     PRIMARY KEY(id)
 );
-
 ALTER TABLE keywords
 ADD FOREIGN KEY (id) REFERENCES uris(id);
